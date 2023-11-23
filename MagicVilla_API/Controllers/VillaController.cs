587ecd458ce0fc5da34f7e4c7704fa4c0b1e10db
AlbertLnz <albert2000_lanza@hotmail.com>
2023-11-23 20:10:35 +0100

    first GET endpoint

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using MagicVilla_API.Modelos;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1, Nombre="Vista a la piscina"},
+                new Villa{Id=2, Nombre="Vista a la playa"}
+            };
+        }
+    }
+}
+
